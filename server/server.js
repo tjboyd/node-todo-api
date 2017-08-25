19df1a97a068f6cb380908d827714959b0fc4b61
11eighty <tjboyd20@gmail.com>
2017-08-25 08:52:48 -0500

    add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {
+  ObjectID
+} = require('mongodb');
 
 var {
   mongoose
@@ -35,6 +38,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+// GET /todos/1234523
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(404).send();
+  });
 });
 
 app.listen(3000, () => {
