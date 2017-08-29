d6afbb5f49f7596b0b007ca84a4ceab5d9221ea5
11eighty <tjboyd20@gmail.com>
2017-08-29 16:18:58 -0500

    added jwt

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,34 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
-
-// var newUser = new User({
-//   email: 'tjboyd20@gmail.com'
-// });
-//
-// newUser.save().then((doc) => {
-//   console.log('Saved User', doc)
-// }, (e) => {
-//   console.log('Unable to save user', e)
-// });
 
 module.exports = {
   User
--- server/server.js
+++ server/server.js
@@ -107,12 +107,25 @@ app.patch('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({
       todo
     });
   }).catch((e) => {
     res.status(400).send();
-  });
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
